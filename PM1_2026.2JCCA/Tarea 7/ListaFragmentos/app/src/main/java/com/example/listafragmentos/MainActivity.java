ad6063b6fb24b82aba4eda159692422a7293ba06
BrayanVM321 <brayanvelazco321@aragon.unam.mx>
2026-05-19 19:55:36 -0600

    Tarea

---
+++ PM1_2026.2JCCA/Tarea 7/ListaFragmentos/app/src/main/java/com/example/listafragmentos/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.listafragmentos;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+//Brayan Velazco Martinez
+
+public class MainActivity extends Activity {
+
+    ListView lista;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lista = findViewById(R.id.listado);
+
+        String[] animales = {
+                "Gato",
+                "Perro",
+                "Vaca"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                animales
+        );
+
+        lista.setAdapter(adapter);
+
+        reemplazarFragmento(new FragmentoGato());
+
+        lista.setOnItemClickListener((parent, view, position, id) -> {
+
+            switch (position) {
+
+                case 0:
+
+                    reemplazarFragmento(
+                            new FragmentoGato()
+                    );
+
+                    break;
+
+                case 1:
+
+                    reemplazarFragmento(
+                            new FragmentoPerro()
+                    );
+
+                    break;
+
+                case 2:
+
+                    reemplazarFragmento(
+                            new FragmentoVaca()
+                    );
+
+                    break;
+            }
+        });
+    }
+
+    public void reemplazarFragmento(Fragment fragmento){
+
+        FragmentManager fragmentManager =
+                getFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .replace(
+                        R.id.contenedorFragmentos,
+                        fragmento
+                )
+                .commit();
+    }
+}
